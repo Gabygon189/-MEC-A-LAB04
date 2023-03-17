6bb0c46155f846dfa063fe7274aecd2eade5bd5e
Laura González <123745097+Gabygon189@users.noreply.github.com>
2023-03-17 10:46:49 -0500

    ENTREGA PARCIAL

---
+++ EPS_Afiliados/src/eps_afiliados/EPS_Afiliados.java
@@ -0,0 +1,18 @@
+import java.util. Scanner;
+import java.util. Timer;
+import java.util. Timer;
+
+        
+package eps_afiliados;
+
+public class EPS_Afiliados {
+    String nombre, apellidos, con_especial ;
+    int edad, turno;
+    char p_complementario;
+    
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
